1885ee0756dcff5e074c68e5a3805a987e4204de
Herbert <herbert.he0229@gmail.com>
2022-03-23 17:41:47 +0800

    feat :sparkles: : Update

--- lua/time.lua
+++ lua/time.lua
@@ -5,8 +5,12 @@ time_translator: 关键词转化
 -- TODO: 需要替换为哈语关键词触发
 local function translator(input, seg)
 	-- 日期关键词处理
+	-- BUG 哈语混排错误
 	if (input == "date") then
 		yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), "(日期)"))
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "(日期)"))
+		-- 仅 RTL 下正确
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y" .. "جۇل" .. "%m" .. "اي" .. "%d" .. "كۇن"), "()"))
 	end
 
 	-- 当前关键词处理
