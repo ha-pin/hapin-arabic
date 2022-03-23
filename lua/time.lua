c1ca81d767e34ae5176a78924f4f38c19ec12e59
Herbert <herbert.he0229@gmail.com>
2022-03-23 13:01:08 +0800

    feat :sparkles: : Add lua script for Hapin Arabic

---
+++ lua/time.lua
@@ -0,0 +1,23 @@
+--[[
+time_translator: 关键词转化
+--]]
+
+-- TODO: 需要替换为哈语关键词触发
+local function translator(input, seg)
+	-- 日期关键词处理
+	if (input == "date") then
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), "(日期)"))
+	end
+
+	-- 当前关键词处理
+	if (input == "now") then
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d %X"), "(当前时间)"))
+	end
+
+	-- 时间关键词处理
+	if(input == "time") then
+		yield(Candidate("time", seg.start, seg._end, os.date("%X"), "(时间)"))
+	end
+end
+
+return translator
---
+++ rime.lua
@@ -0,0 +1,1 @@
+time_translator = require("time")
